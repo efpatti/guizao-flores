29e06f95412e53b2849eb5955929521495cbc2a4
Ilelo <enzo.patti@aluno.senai.br>
2024-03-14 09:50:43 -0300

    changes package.json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,19 +8,25 @@ app.use(cors());
 
 const PORT = 5000;
 
-const options = {
-  method: 'GET',
-  url: 'https://perenual.com/api/species-lists',
-  headers: {
-    Authorization: 'sk-pg5165f2de496ff544578',
-  }
-};
+async function fetchData() {
+  const options = {
+    method: 'GET',
+    url: 'https://perenual.com/api/species-lists',
+    headers: {
+      Authorization: 'sk-pg5165f2de496ff544578',
+    }
+  };
 
-try {
-  const response = await axios.request(options);
-  console.log(response.data);
-} catch (error) {
-  console.error(error);
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-app.listen(`Servidor rodando na porta ${PORT}`)
+fetchData(); // Call the function to fetch data
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
