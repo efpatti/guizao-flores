5ba44793df47de9ce45190b709acc1cee895e7e3
Henrique Barros Pereira <henrique.pereira@aluno.senai.br>
2024-03-14 16:39:27 -0300

    baro confisuion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import { ChakraProvider } from '@chakra-ui/react';
 import NavBar from "./components/NavBar";
-
+import SobreNos from "./pages/aboutus";
 import './App.css';
 
 import Home from "./pages/Homepage";
@@ -13,24 +12,11 @@ function App() {
       <BrowserRouter>
       <Routes>
       <Route index element={<Home />} />
+      <Route path="about" element={<SobreNos />} />
       </Routes>
       </BrowserRouter>
     </ChakraProvider>
 
-=======
-import { ChakraProvider } from "@chakra-ui/react";
-import SobreNos from "./pages/aboutus";
-
-import "./App.css";
-function App() {
-  return (
-    <>
-      <ChakraProvider>
-        <h1>App</h1>
-      </ChakraProvider>
-      <SobreNos />
-    </>
->>>>>>> 936c4b20bd5a9565e3db934d14abe96def8aa782
   );
 }
 
