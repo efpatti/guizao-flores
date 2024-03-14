936c4b20bd5a9565e3db934d14abe96def8aa782
Julia Maria <julia.leite8@aluno.senai.br>
2024-03-14 16:36:32 -0300

    aboutus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import SobreNos from "./pages/aboutus";
+
 import "./App.css";
 function App() {
   return (
@@ -11,17 +11,6 @@ function App() {
       </ChakraProvider>
       <SobreNos />
     </>
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { ChakraProvider } from '@chakra-ui/react'
-import NavBar from "./components/NavBar"
-import './App.css';
-function App() {
-  return (
-    <ChakraProvider>
-      <NavBar />
-    </ChakraProvider>
->>>>>>> fc97df56dfd8ffa673ae2347bdc085b21fab4a03
   );
 }
 
