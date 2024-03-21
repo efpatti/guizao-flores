66bec2ed97b91688bb16abf2fb6f8ae0b122932d
Henrique Barros Pereira <henrique.pereira@aluno.senai.br>
2024-03-21 16:24:10 -0300

    natiruts

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -1,132 +1,7 @@
-<<<<<<< HEAD
 import { CardHeader, CardBody, CardFooter, Stack, Heading, Divider, Card, ButtonGroup, Image, Text, Button } from '@chakra-ui/react';
  import fototeste from'../../img/guizao-flores.png';
  import bannerOrquidea from '../../img/banner-orquidea.webp';
  import bannerOutono from '../../img/banner-outono.webp';
-=======
-<<<<<<< HEAD
-import {
-  CardBody,
-  CardFooter,
-  Stack,
-  Heading,
-  Divider,
-  Card,
-  ButtonGroup,
-  Image,
-  Text,
-  Button,
-  Flex,
-} from "@chakra-ui/react";
-import fototeste from "../../img/banner-abraco.webp";
-import bannerOrquidea from "../../img/banner-orquidea.webp";
-import bannerOutono from "../../img/banner-outono.webp";
-import bannerPascoa from "../../img/banner-pascoa.webp";
-import { React, useState } from "react";
-import Carousel from "react-bootstrap/Carousel";
-import "../Homepage/Homepage.css";
-const buttonStyle = {
-  baseStyle: {
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    borderRadius: "base", // <-- border radius is same for all variants and sizes
-  },
-  // Two sizes: sm and md
-  sizes: {
-    sm: {
-      fontSize: "sm",
-      px: 4, // <-- px is short for paddingLeft and paddingRight
-      py: 3, // <-- py is short for paddingTop and paddingBottom
-    },
-    md: {
-      fontSize: "md",
-      px: 6, // <-- these values are tokens from the design system
-      py: 4, // <-- these values are tokens from the design system
-    },
-  },
-  // Two variants: outline and solid
-  variants: {
-    outline: {
-      border: "2px solid",
-      borderColor: "purple.500",
-      color: "purple.500",
-    },
-    solid: {
-      bg: "purple.500",
-      color: "white",
-    },
-  },
-  // The default size and variant values
-  defaultProps: {
-    size: "md",
-    variant: "outline",
-  },
-};
-
-function Home() {
-  const cardData = [
-    {
-      nome: "arioci",
-      Imagem: fototeste,
-      descricao: "coordenador tecnico do senai",
-      linkedin: "link linkedin",
-    },
-  ];
-
-  return (
-    <>
-      <Carousel className="carrosel">
-        <Carousel.Item>
-          <img className="bannerfoto" src={fototeste} />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="bannerfoto" src={bannerOrquidea} />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="bannerfoto" src={bannerOutono} />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="bannerfoto" src={bannerPascoa} />
-        </Carousel.Item>
-      </Carousel>
-      <h1 className="letreiro">Promoçoes de março</h1>
-      {cardData.map((card, index) => (
-        <Card maxW="sm" key={index}>
-          <CardBody>
-            <Image src={card.Imagem} />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">{card.nome}</Heading>
-              <Text>
-                This sofa is perfect for modern tropical spaces, baroque
-                inspired spaces, earthy toned spaces and for people who love a
-                chic design with a sprinkle of vintage design.
-              </Text>
-              <Text color="blue.600" fontSize="2xl">
-                $450
-              </Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <ButtonGroup spacing="2">
-              <Button {...buttonStyle} colorScheme="blue">
-                Buy now
-              </Button>
-              <Button {...buttonStyle} variant="ghost" colorScheme="blue">
-                Add to cart
-              </Button>
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
-      ))}
-    </>
-  );
-=======
-// // import { CardHeader, CardBody, CardFooter, Stack, Heading, Divider, Card, ButtonGroup, Image, Text, Button } from '@chakra-ui/react';
-// // import fototeste from'../../img/guizao-flores.png';
-// // import bannerOrquidea from '../../img/banner-orquidea.webp';
-// // import bannerOutono from '../../img/banner-outono.webp';
->>>>>>> 0faa345d2573db26fc254b9d66038f9d04a9f03b
  import bannerPascoa from '../../img/banner-pascoa.webp';
  import bannerabraco from '../../img/banner-abraco.webp';
  import { React, useState } from 'react';
@@ -405,21 +280,19 @@ function Home() {
           
       
 
-<<<<<<< HEAD
       
       </div>
       <img className='fotoflor' src={florfofa}/>
         </div>
-        <div className='segunda'>
+        <div className='segunda'></div>
          <img className='fotoflor2' src={florfofa2}/>
         
         <div className='linha2'>
-            <h5 className='titulo-linha'>Use água limpa e fresca:</h5>
+         
           <div className='linha2-corpo'>
          
-      
+         <h5 className='titulo-linha'>Use água limpa e fresca:</h5>
         <p className='textos'>Troque a água do vaso regularmente, a cada dois dias, para evitar a proliferação de bactérias.</p>
-        </div>
         </div>
         </div>
         </div>
@@ -431,14 +304,6 @@ function Home() {
     </>
    
   );
-=======
-      <button className='butao' onClick={handleClick}>Trocar Conteúdo</button>
-
-
-  <div/>
-  </div>);
->>>>>>> refs/remotes/origin/main
->>>>>>> 0faa345d2573db26fc254b9d66038f9d04a9f03b
 }
 
 export default Home;
