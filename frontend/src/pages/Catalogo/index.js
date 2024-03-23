dd7be1cdbe6d6307282d8471215b2c97df0aaf65
efpatti <enzo.patti@aluno.senai.br>
2024-03-23 11:52:12 -0300

    catalogo atualizado com a categoria astromelias imcompleta e a flores secas completa

--- frontend/src/pages/Catalogo/index.js
+++ frontend/src/pages/Catalogo/index.js
@@ -1,21 +1,51 @@
-import { Button, Stack, Center } from "@chakra-ui/react";
-// import { StoreData } from "../../productsStore";
+import { Flex, Text, Stack, Grid, Box, Image } from "@chakra-ui/react";
+import { categoriesStore } from "./data";
 function Catalogo() {
   return (
-    <section>
-      <Center>
-        <Stack direction="row" spacing={4} align="center">
-          {/* {StoreData.map((item, i) => (
-            <div key={i}>
-              <Button colorScheme="teal" size="sm">
-                {item.title}
-              </Button>
-            </div>
-          ))} */}
-          <h1>testing</h1>
-        </Stack>
-      </Center>
-    </section>
+    <Flex mt="10rem" justify="center">
+      <Stack direction="column" spacing="5px" maxW="70%">
+        {categoriesStore.map((category, index) => (
+          <div key={index}>
+            <Text textAlign="center" fontSize="3xl">
+              {category.name}
+            </Text>
+            <Grid templateColumns="repeat(4, 1fr)" gap="10px">
+              {category.products.map((item, i) => (
+                <Box
+                  key={i}
+                  _hover={{ cursor: "pointer", boxShadow: "md" }}
+                  borderRadius="sm"
+                  p="3"
+                >
+                  <Image src={item.img} />
+                  <Text fontWeight="md" color="gray.700">
+                    {item.name}
+                  </Text>
+                  <Grid templateColumns="repeat(2, 1fr)" gap="1px">
+                    <Box>
+                      <Text fontSize="sm" as="s">
+                        de R$ {item.old_price}
+                      </Text>
+                    </Box>
+                    <Box>
+                      <Text fontSize="md" as="b">
+                        {item.price}
+                      </Text>
+                    </Box>
+                    <Box>
+                      <Text fontSize="xs" as="b">
+                        3x <span className="fw-light">de </span>
+                        {(parseFloat(item.price) / 3).toFixed(2)}
+                      </Text>
+                    </Box>
+                  </Grid>
+                </Box>
+              ))}
+            </Grid>
+          </div>
+        ))}
+      </Stack>
+    </Flex>
   );
 }
 
