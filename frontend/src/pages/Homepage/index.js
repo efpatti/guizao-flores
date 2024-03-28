b9d6b204f50a8ac674deeec884935c42300cb215
Henrique Barros Pereira <henrique.pereira@aluno.senai.br>
2024-03-28 16:53:39 -0300

    leprecheta

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -13,11 +13,17 @@ import {
   Flex,
   Box,
 } from "@chakra-ui/react";
-import fototeste from "../../img/guizao-flores.png";
+import fototeste from "../../img/mega-buque-com-girassois-e-rosas-brancas.webp";
 import bannerOrquidea from "../../img/banner-orquidea.webp";
 import bannerOutono from "../../img/banner-outono.webp";
 import bannerPascoa from "../../img/banner-pascoa.webp";
 import bannerabraco from "../../img/banner-abraco.webp";
+import promo2 from "../../img/buque-presente-gerberas-vermelhas.webp";
+import promo33 from "../../img/buque-de-gypsophila-encantada-azul-claro.webp";
+import promo3 from "../../img/flores-secas-roxas.webp";
+import acessorio from "../../img/regador-fofo.jpg";
+import acessorio2 from "../../img/image-removebg-preview (2).png";
+import acessorio3 from "../../img/elefantezur.webp";
 import { React, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "../Homepage/Homepage.css";
@@ -69,28 +75,32 @@ const buttonStyle = {
 function Home() {
   const cardData = [
     {
-      nome: "Titulo flor",
+      nome: "Mega Buquê de Girassóis",
       Imagem: fototeste,
-      Preco: "50,00 R$",
-      Descricao: "descriçao da flor",
+      Preco: "639,00 R$",
+      Odpreco: "720,00 R$",
+      Descricao: " Os girassóis são como pequenos artistas do sol, Suas flores gigantes e brilhantes parecem sorrir para o mundo",
     },
     {
-      nome: "Titulo flor",
-      Imagem: fototeste,
-      Preco: "50,00 R$",
-      Descricao: "descriçao da flor",
+      nome: "Buquê Presente Gérberas Vermelhas",
+      Imagem: promo2,
+      Preco: "87,90 R$",
+      Odpreco: "125,00 R$",
+      Descricao: " é como um jardim de paixão em suas mãos. Cada flor transmite um amor intenso e caloroso.",
     },
     {
-      nome: "Titulo flor",
-      Imagem: fototeste,
-      Preco: "50,00 R$",
-      Descricao: "descriçao da flor",
+      nome: "Buquê Poesia de Flores Secas Roxas",
+      Imagem: promo3,
+      Preco: "89,90 R$",
+      Odpreco: "129,00 R$",
+      Descricao: "é como um pedaço do céu noturno. Cada flor seca preserva a beleza efêmera da natureza.",
     },
     {
-      nome: "Titulo flor",
-      Imagem: fototeste,
-      Preco: "50,00 R$",
-      Descricao: "descriçao da flor",
+      nome: "Buquê de Gypsophila Encantada Azul Claro",
+      Imagem: promo33,
+      Preco: "165,00 R$",
+      Odpreco: "195,90 R$",
+      Descricao: " é como um sonho delicado tornado realidade. Cada pequena flor parece uma estrela celestial. ",
     },
   ];
   const cardApaixonado = [
@@ -121,22 +131,22 @@ function Home() {
   ];
   const cardAcessorio = [
     {
-      nome1: "acessorio",
-      Imagem1: fototeste1,
-      Preco1: "50,00 R$",
-      Descricao1: "descriçao da acessorio",
+      nome1: "Vaso Cerâmica Azul e Preto",
+      Imagem1: acessorio,
+      Preco1: "89,90 R$",
+      Descricao1: " Com um design clássico e elegante, o vaso possui um tom de azul profundo e escuro." ,
     },
     {
-      nome1: "acessorio",
-      Imagem1: fototeste1,
-      Preco1: "50,00 R$",
-      Descricao1: "descriçao da acessorio",
+      nome1: "Vaso de flor transparente",
+      Imagem1: acessorio2,
+      Preco1: "47,90 R$",
+      Descricao1: "Imagine um vaso de flor, feito de vidro delicado que brilha suavemente à luz do sol.",
     },
     {
-      nome1: "acessorio",
-      Imagem1: fototeste1,
-      Preco1: "50,00 R$",
-      Descricao1: "descriçao da acessorio",
+      nome1: "Regador de Tropicana  ",
+      Imagem1: acessorio3,
+      Preco1: "19,99 R$",
+      Descricao1: "regador encantador em forma de pássaro, feito de metal e pintado em cores vibrantes. ",
     },
   ];
   const { colorMode } = useColorMode();
@@ -165,12 +175,13 @@ function Home() {
               <Card maxW="sm">
                 <CardBody>
                   <Image src={card.Imagem} />
-                  <Stack mt="2    " spacing="3">
+                  <Stack mt="2    " spacing="0">
                     <Heading size="md">{card.nome}</Heading>
                     <Text>{card.Descricao}</Text>
                     <Text className="texto-preco" fontSize="2xl">
                       {card.Preco}
                     </Text>
+                    <Text as='s' fontSize="2x1">{card.Odpreco}</Text>
                   </Stack>
                 </CardBody>
                 <Divider />
@@ -223,7 +234,7 @@ function Home() {
             <Col key={index}>
               <Card className="card2" maxW="sm">
                 <CardBody>
-                  <Image src={card.Imagem1} />
+                <Image src={card.Imagem1} />
                   <Stack mt="1  " spacing="3">
                     <Heading size="md">{card.nome1}</Heading>
                     <Text>{card.Descricao1}</Text>
