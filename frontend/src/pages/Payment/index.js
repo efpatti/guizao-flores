95e32c69200e46477dd8e6ff71d1ea5100d3961a
Enrico Ramos Medina <enricomedinaa@gmail.com>
2024-04-02 16:40:51 -0300

    medio

--- frontend/src/pages/Payment/index.js
+++ frontend/src/pages/Payment/index.js
@@ -7,6 +7,7 @@ import {
   HStack,
   Radio,
   FormHelperText,
+  Input,
 } from "@chakra-ui/react";
 // import { Form } from "react-bootstrap"; // Importe Form do react-bootstrap
 import "../Payment/pagamento.css";
@@ -14,6 +15,8 @@ import pix from "../../img/pix.jpeg";
 import guizao from "../../img/debit&credit.jpg";
 import QR from "../../img/qrcode-pix.png";
 import { MdOutlineContentCopy } from "react-icons/md";
+import Timer from "../../Timer";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Payment() {
   const [exibirForms, setExibirForms] = useState(true);
@@ -26,30 +29,64 @@ function Payment() {
     <Flex mt="8.5rem">
       <div className={exibirForms ? "elojob" : "caixa-berde"}>
         {exibirForms ? (
-          <div>
-            <h3 className="textPayment">Seus dados, para finalizar..</h3>
-            <div className="formInfo elojob">
-              <FormControl as="fieldset">
-                <FormLabel as="legend">Escolha forma de pagamento</FormLabel>
-                <RadioGroup defaultValue="Itachi">
-                  <HStack spacing="24px">
-                    <Radio value="Crédito">Crédito</Radio>
-                    <Radio value="Débito">Débito</Radio>
-                  </HStack>
-                </RadioGroup>
-              </FormControl>
+          <>
+            <div>
+              <h3 className="textPayment">Seus dados, para finalizar..</h3>
+              <div className="formInfo elojob">
+                <FormControl as="fieldset">
+                  <FormLabel as="legend">Escolha forma de pagamento</FormLabel>
+                  <RadioGroup defaultValue="Itachi">
+                    <HStack spacing="24px">
+                      <Radio value="Crédito">Crédito</Radio>
+                      <Radio value="Débito">Débito</Radio>
+                    </HStack>
+                  </RadioGroup>
+                </FormControl>
+
+                <div className="irmaozinho">
+                  <FormControl isRequired>
+                    <FormLabel>N° do cartâo</FormLabel>
+                    <Input placeholder="XXXX-XXXX-XXXX-XXXX" maxLength={20} />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel>Nome do titular</FormLabel>
+                    <Input placeholder="Ex: Enzo" />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <Container>
+                      <Row>
+                        <Col className="ultima">
+                          <FormLabel>CVV</FormLabel>
+                          <FormLabel className="data">Data</FormLabel>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col className="ultima">
+                          <Input className="CVV" placeholder="XXX" />
+
+                          <Input className="CVV" placeholder="XX/XX" />
+                        </Col>
+                        <Col></Col>
+                      </Row>
+                    </Container>
+                  </FormControl>
+                </div>
+              </div>
             </div>
-          </div>
+          </>
         ) : (
           <div className="text">
+            <Timer />
             <img className="qrcode" src={QR} width={200} />
             <b className="textQR">Copie o código</b>
 
             <div className="boxCode ">
               <div>
-                <p className="codepix">
-                  <MdOutlineContentCopy />
-                </p>
+                <div className="codepix">
+                  <button className="icon">
+                    <MdOutlineContentCopy />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
