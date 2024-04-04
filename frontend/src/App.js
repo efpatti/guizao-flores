16f9a756fce145d920fc2875fd2313b22c2be770
guisenai <guilherme.martini@aluno.senai.br>
2024-04-04 16:12:53 -0300

    guizaocadastro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { ChakraProvider } from '@chakra-ui/react'
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
+import Cadastro from "./pages/Cadastro";
+
 function App() {
   return (
     <ChakraProvider>
-      <h1>App</h1>
+      <h1 className="text-center">CADASTRO</h1>
+      <Cadastro />
+
+      <br></br>
     </ChakraProvider>
   );
 }
